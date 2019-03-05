0e1328db428fa579d3f29e254133d95a3d6c7e59
DJABHipHop <castellanos.kevin@rocketmail.com>
2019-03-05 14:38:01 -0500

    Creaningup file
    Creaningup file

--- build/config.sh
+++ build/config.sh
@@ -1,6 +1,6 @@
 ccache_use=1
 ccache_dir=/home/"$(whoami)"/ccache
-main_dir=/home/"$(whoami)"
+home_dir=/home/"$(whoami)"
 ccache_size=25G
 
 #cm/du/omni
@@ -8,5 +8,3 @@ rom_type=cm
 
 with_su=1
 jobs=5
-#have_sdclang=0
-#path_sdclang="vendor/extra/toolchain/sdclang-3.8/bin"
